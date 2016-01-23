d6cff05975f9b9285c53d2aa2b3007897f36ac80
michaeljohnorlando <michael.john.orlando@gmail.com>
2016-01-23 06:03:18 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,9 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
+end
